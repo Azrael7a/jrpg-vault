a8d893d1d715c5d7199eca6550a06a84d4067f84
Azrael7a <azrael7a@gmail.com>
2026-07-24 13:02:10 +0200

    Traduire le bouton d’authentification

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
+
 import { createClient } from "@/lib/supabase/server";
+
 import LogoutButton from "./logout-button";
+import { Button } from "./ui/button";
 
 export async function AuthButton() {
   const supabase = await createClient();
 
-  // You can also use getUser() which will be slower.
   const { data } = await supabase.auth.getClaims();
-
   const user = data?.claims;
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Bonjour, {user.email} !
       <LogoutButton />
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
+      <Button asChild size="sm" variant="outline">
+        <Link href="/auth/login">Se connecter</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
+      <Button asChild size="sm" variant="default">
+        <Link href="/auth/sign-up">Créer un compte</Link>
       </Button>
     </div>
   );
